380104252ac2bcfa52f32d4d24145b324509a06e
jhensena <jhensenaquino5@gmail.com>
2022-07-20 09:15:55 -0500

    Initial commit & placard page

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+
+const colors = [
+  { value: 'red', label: 'Red' },
+  { value: 'blue', label: 'Blue' },
+  { value: 'green', label: 'Green' },
+  { value: 'yellow', label: 'Yellow' },
+  { value: 'orange', label: 'Orange' },
+]
+
+function App() {
+  return (
+    <div className="App">
+      <div className="color-area">
+        {[1, 2, 3].map((idx) => (
+          <select name={`color_${idx}`}>
+            { colors.map((color) => (
+              <option value={color.value}>{color.label}</option>
+            )) }
+          </select>
+        ))}
+      </div>
+      <div className="container">
+        <div className="input-field">
+          <label>JOB:</label>
+          <input name="job" />
+        </div>
+        <div className="input-field">
+          <label>SO #:</label>
+          <input name="so" />
+        </div>
+        <div className="input-field">
+          <label>ITEM ID:</label>
+          <input name="item_id" />
+        </div>
+        <div className="input-field">
+          <label>REC'D BY:</label>
+          <input name="rec_by" />
+        </div>
+        <div className="footer">
+          <div>RECEIVING FORM</div>
+          <div>ARIZONA RESTAURANT SUPPLY</div>
+          <div>V2R1</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
